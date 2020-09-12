83b830a5939ca87c2e88e9895f2beab18885e353
Daniel Morgan <dcolinmorgan@gmail.com>
2020-09-12 12:09:24 -0400

    test nonCG regions per Abhi

--- camb_motif_pipeline.sh
+++ camb_motif_pipeline.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #$ -cwd
-## bash data/MotifPipeline/ENCODE/camb_motif_pipeline.sh -c'A549 K562 GM12878 SKNSH HepG2 HeLa' -g'HOXA7 RFX6 ZBTB49'
+## bash data/MotifPipeline/ENCODE/camb_motif_pipeline.sh -c'A549 K562 GM12878 SKNSH HepG2 HeLa' -g'HOXA7 RFX6 ZBTB4'
 usage=""$sthlm_motif_pipeline" [-h] [-cgo] -- function calling bedtools2 to calculate the intersect for ENCODE methylation benchmark with arbitrary buffer length
 
 where:
@@ -60,8 +60,9 @@ if [[ $* == *-g* ]];then
 
       CG=$(eval wc -l $outdir/CG/$cell"_"$gene | cut -f1 --delimiter=' ')
       nonCG=$(eval wc -l $outdir/nonCG/$cell"_"$gene | cut -f1 --delimiter=' ')
+      total=$(eval wc -l $outdir/benchmark_tmp/tmp2.txt| cut -f1 --delimiter=' ')
       
-      echo $CG $nonCG $tf $gene $cell
+      echo $CG $nonCG $total $tf $gene $cell
 
       rm $outdir/benchmark_tmp/tmp3.txt $outdir/benchmark_tmp/tmp2.txt $outdir/benchmark_tmp/tmp4.txt $outdir/benchmark_tmp/tmp1.txt
 
