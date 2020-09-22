c50481baf0689386880586407a9888c7df8362ea
dcolinmorgan <redmo@channing.harvard.edu>
2020-09-22 14:00:43 -0400

    limit expanded motif window to 0 buffer

--- camb_motif_pipeline_beta.sh
+++ camb_motif_pipeline_beta.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #$ -cwd
-## bash mili_benchmark/camb_motif_pipeline_gamma.sh -b500 -c'A549 K562 GM12878 SKNSH HepG2 HeLa' -o'../../../pc/redmo/data/MotifPipeline/'
+## bash mili_benchmark/camb_motif_pipeline_beta.sh -b5 -c'A549 K562 GM12878 SKNSH HepG2 HeLa' -o'../../../pc/redmo/data/MotifPipeline/'
 usage=""$camb_motif_pipeline" [-h] [-bco] -- function calling bedtools2 to calculate the intersect for ENCODE methylation benchmark with arbitrary buffer length
 
 where:
@@ -39,14 +39,13 @@ me=$(eval "basename "$0" | cut -d . -f1")
 tfdb='data/MotifPipeline/ENCODE/Homo_sapiens_motifinfo.txt'
 motifdir='../rekrg/MotifScans/MotifScans/hg38/'
 motifs=$(ls $motifdir*)
-bench="../../../pc/redmo/data/MotifPipeline/"
+bench="../../../pc/redmo/data/expand_base0_MotifPipeline/"
 bench=$bench$me$buffer
 
 # rm -rf bench
 # mkdir bench
 rm -rf $bench
 mkdir -p $bench/test
-# mkdir -p $bench/red
 # bench="benchmark_"
 # bench=$bench$me$buffer
 
@@ -54,13 +53,11 @@ mkdir -p $bench/test
 # mkdir benchmark_$me$buffer
 for cell in $cells
 do
-  printf "...running wgbs intersection with array for the "$cell" cell line \n"
+  # printf "...running wgbs intersection with array for the "$cell" cell line \n"
 
-  eval "~/../rekrg/Tools/bedtools2/bin/bedtools intersect  -wa -wb -a data/MotifPipeline/ENCODE/wgbsin/"$cell"both.txt -b data/MotifPipeline/ENCODE/methyl_array/crossQC/"$cell"_MeArrayHG38c.txt" |cut -f1,2,3,4,5,9,10 > $bench/overlap_tmpA_$cell"_"$buffer$out_fmt
-  printf "...intersecting methylation information with ChIP for the "$cell" cell line \n"
-  eval "~/../rekrg/Tools/bedtools2/bin/bedtools intersect -wao -a $bench/overlap_tmpA_"$cell"_"$buffer$out_fmt" -b data/MotifPipeline/remap/"$cell"_spRE2020.txt " >  $bench/overlap_tmpB_$cell"_"$buffer$out_fmt #$bench/$cell"_"$gene
-  sed -i.bak $'s/\t\t/\t/' $bench/overlap_tmpB_$cell"_"$buffer$out_fmt
-  cut -f1,2,3,4,5,11 $bench/overlap_tmpB_$cell"_"$buffer$out_fmt > $bench/overlap_tmpBB_$cell"_"$buffer$out_fmt
+  # eval "~/../rekrg/Tools/bedtools2/bin/bedtools intersect  -wa -wb -a data/MotifPipeline/ENCODE/wgbsin/"$cell"both.txt -b data/MotifPipeline/ENCODE/methyl_array/crossQC/"$cell"_MeArrayHG38c.txt" |cut -f1,2,3,4,5,9,10 > $bench/overlap_tmpA_$cell"_"$buffer$out_fmt
+  # printf "...intersecting methylation information with ChIP for the "$cell" cell line \n"
+
 # done
 # fi
 
@@ -75,30 +72,30 @@ do
     if [ $buffer != 0 ];then
       printf "...adding +/-"$buffer" bp buffer around motif region for "$gene" in the "$cell" cell line \n"
       eval "~/../rekrg/Tools/bedtools2/bin/bedtools slop  -i $bench/overlap_tmpD_$cell"_"$buffer$out_fmt -g ~/../rekrg/Tools/bedtools2/genomes/human.hg38.genome -r "$buffer" -l "$buffer"" > $bench/overlap_tmpC_$cell"_"$buffer$out_fmt
+      eval "~/../rekrg/Tools/bedtools2/bin/bedtools intersect -wao -a $bench/overlap_tmpC_"$cell"_"$buffer$out_fmt" -b data/MotifPipeline/remap/"$cell"_spRE2020.txt " >  $bench/overlap_tmpB_$cell"_"$buffer$out_fmt #$bench/$cell"_"$gene
+      sed -i.bak $'s/\t\t/\t/' $bench/overlap_tmpB_$cell"_"$buffer$out_fmt
+      cut -f1,2,3,4,5,11 $bench/overlap_tmpB_$cell"_"$buffer$out_fmt > $bench/overlap_tmpBB_$cell"_"$buffer$out_fmt
+      eval "~/../rekrg/Tools/bedtools2/bin/bedtools intersect -wa -a $bench/overlap_tmpBB_$cell"_"$buffer$out_fmt -b $bench/overlap_tmpBB_$cell"_"0$out_fmt " >  $outdir/test/$cell"_"$gene
+
     else
       printf "...no buffered motif for "$gene" in the "$cell" cell line \n"
       cat $bench/overlap_tmpD_$cell"_"$buffer$out_fmt > $bench/overlap_tmpC_$cell"_"$buffer$out_fmt
+      eval "~/../rekrg/Tools/bedtools2/bin/bedtools intersect -wao -a $bench/overlap_tmpC_"$cell"_"$buffer$out_fmt" -b data/MotifPipeline/remap/"$cell"_spRE2020.txt " >  $bench/overlap_tmpB_$cell"_"$buffer$out_fmt #$bench/$cell"_"$gene
+      sed -i.bak $'s/\t\t/\t/' $bench/overlap_tmpB_$cell"_"$buffer$out_fmt
+      cut -f1,2,3,4,5,11 $bench/overlap_tmpB_$cell"_"$buffer$out_fmt > $bench/overlap_tmpBB_$cell"_"$buffer$out_fmt
     fi
     printf "intersecting buffered motif with "$gene" methyl-predictions in the "$cell" cell line \n"
-    
 
-    eval "~/../rekrg/Tools/bedtools2/bin/bedtools intersect -wa -wb -a $bench/overlap_tmpC_$cell"_"$buffer$out_fmt -b $bench/overlap_tmpBB_"$cell"_"$buffer$out_fmt"" > $bench/overlap_tmpCC_$cell"_"$buffer$out_fmt #bench/overlap_tmpE_$cell"_"$buffer$out_fmt
+    eval "~/../rekrg/Tools/bedtools2/bin/bedtools intersect -wa -wb -a $bench/overlap_tmpC_$cell"_"$buffer$out_fmt -b $bench/overlap_tmpBB_"$cell"_"$buffer$out_fmt"" > $bench/$cell"_"$gene #bench/overlap_tmpE_$cell"_"$buffer$out_fmt
     
-    if [ $buffer != 0 ];then
-      cut -f6,7,8 $bench/overlap_tmpCC_$cell"_0"$out_fmt > $bench/tmp33.txt
-    printf "...reducing buffered ("$buffer"bp) motif regions to only those in common w/ 0 \n"
-    eval "~/../rekrg/Tools/bedtools2/bin/bedtools intersect -wa -a $bench/overlap_tmpCC_$cell"_"$buffer$out_fmt -b $bench/tmp33.txt " >  $bench/$cell"_"$gene
-    rm $bench/overlap_tmpD_$cell"_"$buffer$out_fmt  $bench/overlap_tmpBB_$cell"_"$buffer$out_fmt $bench/overlap_tmpB_$cell"_"$buffer$out_fmt
-    rm $bench/overlap_tmpC_$cell"_"$buffer$out_fmt $bench/overlap_tmpCC_$cell"_"$buffer$out_fmt tmp33.txt
-    else
-      cat $bench/overlap_tmpCC_$cell"_"$buffer$out_fmt $bench/overlap_tmpCC_$cell"_0"$out_fmt >  $bench/$cell"_"$gene
-    fi
+    # printf "...reducing buffered ("$buffer"bp) motif regions to only those in common w/ 0 \n"
+    # eval "~/../rekrg/Tools/bedtools2/bin/bedtools intersect -wa -a bench/overlap_tmpE_$cell"_"$buffer$out_fmt -b data/MotifPipeline/sthlm_motif_0_QCbeta/$cell"_"$gene " >  $bench/red/$cell"_"$gene
+    # rm $bench/$cell"_"$gene bench/overlap_tmpD_$cell"_"$buffer$out_fmt  bench/overlap_tmE_$cell"_"$buffer$out_fmt bench/overlap_tmpC_$cell"_"$buffer$out_fmt
 
 done
 done
 printf "intersected all cell line methyl-methyl-chip info successfully! \n \n"
-
-
+s
 # done
 # find $bench/test/ -size +5G -delete
 
