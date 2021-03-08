523a38acc5c25bb3f852e8250d89a8fe127fc7d5
dcolinmorgan <redmo@channing.harvard.edu>
2021-03-08 15:37:37 -0500

    update auroc calc

--- scripts/shell/cle_motif_bench.sh
+++ scripts/shell/cle_motif_bench.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #$ -cwd
-## bash mili_benchmark/scripts/shell/cle_motif_bench.sh -s100 -e200
+## bash mili_benchmark/scripts/shell/cle_motif_bench.sh -b100 -s400 -e500
 usage=""$cle_motif_bench" [-h] [-bseo] -- function calling bedtools2 to calculate the intersect for ENCODE methylation benchmark with arbitrary buffer length
 where:
   -h  show this help text
@@ -20,7 +20,7 @@ while getopts ":h:b:s:e:o:" opt; do
       ;;
     e) end="$OPTARG" #{OPTARG:-"data/MotifPipeline/camb_motif"$buffer"QC"}
       ;;
-    o) end="$OPTARG" #{OPTARG:-"data/MotifPipeline/camb_motif"$buffer"QC"}
+    o) output="$OPTARG" #{OPTARG:-"data/MotifPipeline/camb_motif"$buffer"QC"}
       ;;
     \?) echo "Invalid option -$OPTARG" >&3
       ;;
@@ -31,6 +31,13 @@ done
 motifdir='/udd/rekrg/MotifScans/MotifScans/hg38'  ## chr start stop pwm per gene name/
 motiffiles=$(ls -U $motifdir/* |sed -n "$start,$end p")
 
+output='../../d/tmp/redmo/bench/'
+output=$output$buffer
+# rm -rf $output
+# mkdir $output
+if [ ! -d "$output" ]; then
+  mkdir $output
+fi
 # echo $depth
 # rm -rf ~/counts.txt
 # echo $motiffiles
@@ -38,37 +45,46 @@ for tf in $motiffiles # ${motiffiles[@]:$start:$end} #
 do
   # TF=$(eval "echo "$tf" | cut -d / -f6| cut -d _ -f1")
   TF=$(eval "cut -f2 $tf"|head -2 | tail -1|cut -f1 -d _|sed 's/[)(]//g')
-  awk '{print($3,"\t",$4,"\t",$5,"\t",$7,"\t",$2)}' $tf |tail -n +2 > '../../d/tmp/redmo/bench/alt/'$TF'A.txt' #>mega_motif.txt
-  cut -f1 '../../d/tmp/redmo/bench/alt/'$TF'A.txt' -d _ > '../../d/tmp/redmo/bench/alt/'$TF'B.txt' # cut -f3,4,5,7,3 $tf
-  tr -d ' ' < '../../d/tmp/redmo/bench/alt/'$TF'B.txt' > '../../d/tmp/redmo/bench/alt/'$TF'C.txt'
-  awk  '$3!=""' '../../d/tmp/redmo/bench/alt/'$TF'C.txt' > '../../d/tmp/redmo/bench/alt/'$TF'D.txt'
-  tr -d ' ' < '../../d/tmp/redmo/bench/alt/'$TF'D.txt' > '../../d/tmp/redmo/bench/alt/'$TF'E.txt'
+  awk '{print($3,"\t",$4,"\t",$5,"\t",$7,"\t",$2)}' $tf |tail -n +2 > $output/$TF'_A.txt' #>mega_motif.txt
+  cut -f1 $output/$TF'_A.txt' -d _ > $output/$TF'_B.txt' # cut -f3,4,5,7,3 $tf
+  tr -d ' ' < $output/$TF'_B.txt' > $output/$TF'_C.txt'
+  awk  '$3!=""' $output/$TF'_C.txt' > $output/$TF'_D.txt'
+  tr -d ' ' < $output/$TF'_D.txt' > $output/$TF'_E.txt'
 
   printf ""$TF"\n"
   # cut -f3,5,6,13 ../../d/tmp/redmo/bench/hg38_Tss_coordinates.csv > ../../d/tmp/redmo/bench/hg38_Tss_coordinates01.txt
   # cat ../../d/tmp/redmo/bench/hg38_Tss_coordinates01.txt | tail -n +2 >../../d/tmp/redmo/bench/hg38_tss_coord.txt
-  eval "~/../../../../udd/rekrg/Tools/bedtools2/bin/bedtools intersect -wa -wb -a ../../d/tmp/redmo/bench/alt/"$TF"E.txt -b ../../d/tmp/redmo/bench/hg38_tss_coord.txt > ../../d/tmp/redmo/bench/alt/"$TF"_TSS.txt"
-  # if buffer!=0;then
-  #   eval "~/../../../../udd/rekrg/Tools/bedtools2/bin/bedtools slop  -i mega_motif01.txt -g ../rekrg/Tools/bedtools2/genomes/human.hg38.genome -r buffer -l buffer > ../../d/tmp/redmo/bench/alt/megaSlop_"$buffer".txt"
-  #   sed -i.bak 's/\t\t/\t/' ../../d/tmp/redmo/bench/alt/megaSlop_"$buffer".txt
-  #     cut -f1,2,3,4,5,11 ../../d/tmp/redmo/bench/alt/megaSlop_"$buffer".txt > ../../d/tmp/redmo/bench/alt/megaSlop_"$buffer".txt
- #      eval "~/../rekrg/Tools/bedtools2/bin/bedtools intersect -wa -a $bench/overlap_tmpBB_$cell"_"$buffer$out_fmt -b $bench/overlap_tmpBB_$cell"_"0$out_fmt " >  $outdir/test/$cell"_"$gene
+  eval "~/../../../../udd/rekrg/Tools/bedtools2/bin/bedtools intersect -wa -wb -a " $output/$TF"_E.txt -b ../../d/tmp/redmo/bench/hg38_tss_coord.txt" > $output/$TF"_TSS.txt"
+  
+  # ../../../../udd/rekrg/Tools/bedtools2/bin/bedtools intersect -wao -a data/MotifPipeline/ENCODE/wgbsin/GM12878both.txt -b data/MotifPipeline/remap/GM12878_spRE2020.txt > GM12878_BS_Ch.txt
 
-  #     eval "~/../../../../udd/rekrg/Tools/bedtools2/bin/bedtools intersect -wao -a ../../d/tmp/redmo/bench/alt/Motif_TSS.txt -b ../../d/tmp/redmo/bench/GM12878_BS_Ch.txt > Motif_TSS_BS_Ch.txt"
- 
-  # else
-    eval "~/../../../../udd/rekrg/Tools/bedtools2/bin/bedtools intersect -wao -a ../../d/tmp/redmo/bench/alt/Motif_TSS.txt -b ../../d/tmp/redmo/bench/GM12878_BS_Ch01.txt "> "../../d/tmp/redmo/bench/alt/"$TF".txt"
-  # fi
-  rm -rf "../../d/tmp/redmo/bench/alt/"$TF"_TSS.txt" "../../d/tmp/redmo/bench/alt/"$TF"A.txt" "../../d/tmp/redmo/bench/alt/"$TF"B.txt" "../../d/tmp/redmo/bench/alt/"$TF"C.txt" "../../d/tmp/redmo/bench/alt/"$TF"D.txt" "../../d/tmp/redmo/bench/alt/"$TF"E.txt"
+  # sed -i.bak 's/\t\t/\t/' GM12878_BS_Ch.txt
+  # cut -f1,2,3,4,5,9 GM12878_BS_Ch.txt > GM12878_BS_Ch0.txt
+  # cat GM12878_BS_Ch0.txt | tr "." 0 >GM12878_BS_Ch01.txt
+
+  if [ $buffer != 0 ];then
+    printf "...adding +/-"$buffer" bp buffer around motif region for "$TF" \n"
+    eval "~/../../../../udd/rekrg/Tools/bedtools2/bin/bedtools slop  -i " $output/$TF"_TSS.txt -g ../rekrg/Tools/bedtools2/genomes/human.hg38.genome -r buffer -l buffer "> $output/$TF"_megaSlop_"$buffer".txt"
+    
+    printf "intersecting buffered motif with "$TF" WGBS-predictions with "$buffer" bp buffer \n"
+    eval "~/../../../../udd/rekrg/Tools/bedtools2/bin/bedtools intersect -wao -a " $output/$TF"_megaSlop_"$buffer".txt -b ../../d/tmp/redmo/bench/GM12878_BS_Ch01.txt "> $output/$TF".txt"
+    rm -rf $output"megaSlop_"$buffer"_B.txt"
+  else
+    printf "intersecting buffered motif with "$TF" WGBS-predictions with 0 buffer \n"
+    eval "~/../../../../udd/rekrg/Tools/bedtools2/bin/bedtools intersect -wao -a " $output/$TF"_TSS.txt -b ../../d/tmp/redmo/bench/GM12878_BS_Ch01.txt "> $output/$TF".txt"
+  fi
+  rm -rf $output/$TF"_TSS.txt" $output/$TF"_megaSlop_"$buffer".txt" $output/$TF"_A.txt" $output/$TF"_B.txt" $output/$TF"_C.txt" $output/$TF"_D.txt" $output/$TF"_E.txt"
 done
 # done
-c
 
-# source /proj/relibs/relib00/conda/bin/activate
-# source activate mypy3
-# chmod +x mili_benchmark/scripts/python/milipede_red.py
-# python mili_benchmark/scripts/python/milipede_red.py
 
+source /proj/relibs/relib00/conda/bin/activate
+source activate mypy3
+chmod +x mili_benchmark/scripts/python/milipede_red.py
+python mili_benchmark/scripts/python/milipede_red.py -i $output
+
+
+####TEST #####
 
 
 
